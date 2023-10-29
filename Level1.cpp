c2287558b067e1abb4c55131ae1ddfb2fddaa6c4
kazz <gang9696@gmail.com>
2023-10-29 23:00:10 +0900

    test

--- Level1.cpp
+++ Level1.cpp
@@ -16,3 +16,17 @@ vector<int> solution(long long n) {
     return answer;
 }
 
+// 콜라츠 추측. ? : 기억하기
+int solution(int num) {
+    int answer = 0;
+    long long n{ num };
+    while (n != 1)
+    {
+        n % 2 == 0 ? n /= 2 : n = 3 * n + 1;
+        answer++;
+
+        if (answer == 500) { answer = -1; break; }
+
+    }
+    return answer;
+}
